047fe2b8c50680c21dfb229c76339ab459f18b98
KenStarry <starrycodes@gmail.com>
2023-06-14 14:19:01 +0300

    Home page redesign

--- lib/features/feature_home/presentation/home_screen.dart
+++ lib/features/feature_home/presentation/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flow/core/utils/extensions.dart';
+import 'package:flow/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,9 +13,24 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Flow"),
+    return AnnotatedRegion(
+      value: const SystemUiOverlayStyle(
+          systemNavigationBarColor: bgColor,
+          systemNavigationBarIconBrightness: Brightness.light),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Flow").addGradient,
+          centerTitle: true,
+          systemOverlayStyle: const SystemUiOverlayStyle(
+              statusBarColor: bgColor,
+              statusBarIconBrightness: Brightness.light),
+        ),
+        backgroundColor: bgColor,
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Text("Hello"),
+        )),
       ),
     );
   }
