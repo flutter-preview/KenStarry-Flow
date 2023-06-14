fec6c9e55af8cea16d6a68df3535a163b2f01e06
KenStarry <starrycodes@gmail.com>
2023-06-14 20:35:53 +0300

    Modified app colors

--- lib/features/feature_home/presentation/components/song_card.dart
+++ lib/features/feature_home/presentation/components/song_card.dart
@@ -12,61 +12,64 @@ class SongCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Theme.of(context).primaryColorDark,
-      ),
-      child: Row(
-        children: [
-          //  image
-          Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
-            child: QueryArtworkWidget(
-              id: song.id,
-              type: ArtworkType.AUDIO,
-              artworkFit: BoxFit.cover,
-              artworkWidth: double.infinity,
-              artworkHeight: double.infinity,
-              artworkBorder: BorderRadius.circular(8),
-              nullArtworkWidget: const Icon(
-                Icons.music_note,
-                color: accent,
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Theme.of(context).primaryColorDark,
+        ),
+        child: Row(
+          children: [
+            //  image
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+              child: QueryArtworkWidget(
+                id: song.id,
+                type: ArtworkType.AUDIO,
+                artworkFit: BoxFit.cover,
+                artworkWidth: double.infinity,
+                artworkHeight: double.infinity,
+                artworkBorder: BorderRadius.circular(8),
+                nullArtworkWidget: const Icon(
+                  Icons.music_note,
+                  color: accent,
+                ),
               ),
             ),
-          ),
 
-          const SizedBox(
-            width: 12,
-          ),
-
-          //  songname
-          Flexible(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  song.displayNameWOExt,
-                  style: Theme.of(context).textTheme.bodyMedium,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                ),
-                const SizedBox(height: 8,),
-                Text(
-                  "${song.artist}",
-                  style: Theme.of(context).textTheme.bodySmall,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                )
-              ],
+            const SizedBox(
+              width: 12,
             ),
-          ),
 
-        ],
+            //  songname
+            Flexible(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    song.displayNameWOExt,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(height: 8,),
+                  Text(
+                    "${song.artist}",
+                    style: Theme.of(context).textTheme.bodySmall,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                ],
+              ),
+            ),
+
+          ],
+        ),
       ),
     );
   }
