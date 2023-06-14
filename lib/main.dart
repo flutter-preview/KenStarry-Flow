7b4cbf030ffe4cf6d807d2ba6acfef92e86f0c52
KenStarry <starrycodes@gmail.com>
2023-06-14 14:37:39 +0300

    Added player controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flow/features/feature_home/presentation/controller/player_controller.dart';
 import 'package:flow/features/feature_home/presentation/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -6,9 +7,21 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    Get.put(PlayerController());
+  }
+  
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
