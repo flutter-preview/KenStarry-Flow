a67f6bfe87db566cdd61deec86cd70ef4f9aea57
KenStarry <starrycodes@gmail.com>
2023-06-14 15:09:11 +0300

    Added logic to get storage permission from user

--- lib/features/feature_home/presentation/controller/player_controller.dart
+++ lib/features/feature_home/presentation/controller/player_controller.dart
@@ -4,22 +4,27 @@ import 'package:on_audio_query/on_audio_query.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class PlayerController extends GetxController {
-
   final audioQuery = OnAudioQuery();
+
+  final isPermissionGranted = false.obs;
+
+  Future<List<SongModel>> getSongs() async {
+    return await OnAudioQuery().querySongs(
+        ignoreCase: true,
+        orderType: OrderType.ASC_OR_SMALLER,
+        sortType: null,
+        uriType: UriType.EXTERNAL);
+  }
 
   checkPermission() async {
     //  request access to storage
     var storagePermission = await Permission.storage.request();
 
     if (storagePermission.isGranted) {
-      return audioQuery.querySongs(
-        ignoreCase: true,
-        orderType: OrderType.ASC_OR_SMALLER,
-        sortType: null,
-        uriType: UriType.EXTERNAL
-      );
+      isPermissionGranted.value = true;
     } else {
       checkPermission();
+      isPermissionGranted.value = false;
     }
   }
 }
--- lib/features/feature_home/presentation/home_screen.dart
+++ lib/features/feature_home/presentation/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flow/core/utils/extensions.dart';
+import 'package:flow/features/feature_home/presentation/controller/player_controller.dart';
 import 'package:flow/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+import 'package:get/get.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,8 +14,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late final PlayerController _playerController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _playerController = Get.find<PlayerController>();
+  }
+
   @override
   Widget build(BuildContext context) {
+    _playerController.checkPermission();
+
     return AnnotatedRegion(
       value: const SystemUiOverlayStyle(
           systemNavigationBarColor: bgColor,
@@ -29,7 +43,27 @@ class _HomeScreenState extends State<HomeScreen> {
         body: SafeArea(
             child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Text("Hello"),
+          child: _playerController.isPermissionGranted.value
+              ? FutureBuilder<List<SongModel>>(
+                  future: _playerController.getSongs(),
+                  builder: (context, snapshot) {
+                    if (snapshot.data == null) {
+                      return const Text(
+                        "No data found",
+                        style: TextStyle(color: Colors.white),
+                      );
+                    }
+
+                    if (snapshot.data!.isEmpty) {
+                      return const Text("Empty data",
+                          style: TextStyle(color: Colors.white));
+                    }
+                    return Container(
+                      color: Colors.red,
+                    );
+                  },
+                )
+              : Text("Permission not granted"),
         )),
       ),
     );
