6073de987ff2eb33ee0ef47a4e6417e56fd17650
KenStarry <starrycodes@gmail.com>
2023-06-14 14:32:56 +0300

    Added player controller

--- lib/features/feature_home/presentation/controller/player_controller.dart
+++ lib/features/feature_home/presentation/controller/player_controller.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class PlayerController extends GetxController {
 
+  final audioQuery = OnAudioQuery();
+
+  checkPermission() async {
+    //  request access to storage
+    var storagePermission = await Permission.storage.request();
+
+    if (storagePermission.isGranted) {
+      return audioQuery.querySongs(
+        ignoreCase: true,
+        orderType: OrderType.ASC_OR_SMALLER,
+        sortType: null,
+        uriType: UriType.EXTERNAL
+      );
+    } else {
+      checkPermission();
+    }
+  }
 }
