f716cc0abb8c893618944dab346a0aaf2a392b5b
KenStarry <starrycodes@gmail.com>
2023-06-15 13:05:32 +0300

    Player Screen UI redesign

--- lib/features/feature_player/presentation/player_screen.dart
+++ lib/features/feature_player/presentation/player_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 
 class PlayerScreen extends StatelessWidget {
@@ -9,26 +10,41 @@ class PlayerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Wrap(
+      alignment: WrapAlignment.center,
+      runSpacing: 24,
+      crossAxisAlignment: WrapCrossAlignment.center,
       children: [
-        Column(
-          children: [
-            //  Album Art
-            UnconstrainedBox(
-              child: Container(
-                  width: 280,
-                  height: 280,
-                  child: QueryArtworkWidget(
-                    id: song.id,
-                    type: ArtworkType.AUDIO,
-                    artworkWidth: double.infinity,
-                    artworkHeight: double.infinity,
-                    artworkBorder: BorderRadius.circular(16),
-                    artworkFit: BoxFit.cover,
-                    artworkQuality: FilterQuality.high,
-                    quality: 100,
-                  )),
-            )
-          ],
+        UnconstrainedBox(
+          child: Container(
+              width: 280,
+              height: 280,
+              child: QueryArtworkWidget(
+                id: song.id,
+                type: ArtworkType.AUDIO,
+                artworkWidth: double.infinity,
+                artworkHeight: double.infinity,
+                artworkBorder: BorderRadius.circular(16),
+                artworkFit: BoxFit.cover,
+                artworkQuality: FilterQuality.high,
+                nullArtworkWidget: Icon(
+                  Icons.music_note_outlined,
+                  size: 120,
+                  color: Theme.of(context).iconTheme.color,
+                ),
+                quality: 100,
+              )),
+        ),
+        //  song Name
+        Container(
+          width: double.infinity,
+          height: 50,
+          padding: const EdgeInsets.symmetric(horizontal: 48),
+          child: Marquee(
+            text: song.displayNameWOExt,
+            style: Theme.of(context).textTheme.bodyLarge,
+            pauseAfterRound: const Duration(seconds: 1),
+            crossAxisAlignment: CrossAxisAlignment.center,
+          ),
         )
       ],
     );
