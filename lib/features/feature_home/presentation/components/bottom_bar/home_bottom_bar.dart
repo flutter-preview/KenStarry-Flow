577001cc63394b6ac54b01be508a39ec973490da
KenStarry <starrycodes@gmail.com>
2023-06-16 14:44:50 +0300

    Added Bottom navigation bar

--- lib/features/feature_home/presentation/components/bottom_bar/home_bottom_bar.dart
+++ lib/features/feature_home/presentation/components/bottom_bar/home_bottom_bar.dart
@@ -1,3 +1,4 @@
+import 'package:flow/features/feature_home/domain/model/player_states.dart';
 import 'package:flow/features/feature_home/presentation/components/bottom_bar/bottom_bar_item.dart';
 import 'package:flow/features/feature_home/presentation/controller/player_controller.dart';
 import 'package:flutter/material.dart';
@@ -22,13 +23,13 @@ class _HomeBottomBarState extends State<HomeBottomBar> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+      margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
           color: Theme.of(context).scaffoldBackgroundColor,
           boxShadow: [
             BoxShadow(
-              color: Colors.grey.withOpacity(0.1),
+              color: Colors.grey.withOpacity(0.2),
               spreadRadius: 10,
               blurRadius: 30,
             )
@@ -38,27 +39,110 @@ class _HomeBottomBarState extends State<HomeBottomBar> {
         child: Obx(
           () => Wrap(
             alignment: WrapAlignment.start,
-            runSpacing: 16,
+            runSpacing: 0,
             children: [
               //  Bottom Nav Currently playing music
               _playerController.songs.isNotEmpty
-                  ? Row(
+                  ? Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        //  track name
-                        Flexible(
-                          child: Text(
-                            _playerController
-                                .songs[_playerController
-                                    .currentPlayingSongIndex.value!].displayNameWOExt,
-                            style: Theme.of(context).textTheme.bodySmall,
-                            overflow: TextOverflow.ellipsis,
-                          ),
+                        //  Track name
+                        Row(
+                          children: [
+                            //  track name
+                            Expanded(
+                              child: Text(
+                                _playerController
+                                    .songs[_playerController
+                                        .currentPlayingSongIndex.value!]
+                                    .displayNameWOExt,
+                                style: Theme.of(context).textTheme.bodySmall,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+
+                            //  play icon
+                            IconButton(
+                                onPressed: () {},
+                                enableFeedback: false,
+                                icon: Icon(
+                                    _playerController.playerState.value ==
+                                            PlayerStates.playing
+                                        ? Icons.pause
+                                        : Icons.play_arrow_rounded,
+                                size: 24,
+                                color: Theme.of(context).iconTheme.color,))
+                          ],
                         ),
-                        
-                        //  play icon
-                        IconButton(onPressed: (){}, icon: const Icon(Icons.play_arrow_rounded))
-                        //  seekbar
                         //  elapsed time
+                        Row(
+                          children: [
+                            //  slider and elapsed time
+                            Expanded(
+                              flex: 2,
+                              child: Row(
+                                children: [
+                                  Text(
+                                    _playerController.position.value.toString(),
+                                    style: Theme.of(context).textTheme.bodySmall,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  //  slider
+                                  Expanded(
+                                    child: SliderTheme(
+                                      data: SliderTheme.of(context).copyWith(
+                                          trackHeight: 5,
+                                          thumbColor: Colors.transparent,
+                                          thumbShape: SliderComponentShape.noThumb),
+                                      child: Slider(
+                                        value: _playerController.sliderValue.value,
+                                        min: const Duration(seconds: 0)
+                                            .inSeconds
+                                            .toDouble(),
+                                        max: _playerController.maxSlider.value,
+                                        onChanged: (newValue) {
+                                          _playerController.seekSong(
+                                              seconds: newValue.toInt());
+                                          newValue = newValue;
+
+                                          if (_playerController.sliderValue.value ==
+                                              _playerController.maxSlider.value) {
+                                            //  go to next song
+                                            _playerController.playSong(
+                                                path: _playerController
+                                                    .songs[_playerController
+                                                            .currentPlayingSongIndex
+                                                            .value! +
+                                                        1]
+                                                    .uri!,
+                                                index: _playerController
+                                                        .currentPlayingSongIndex
+                                                        .value! +
+                                                    1);
+                                          }
+                                        },
+                                        thumbColor: Theme.of(context).primaryColor,
+                                        activeColor: Theme.of(context).primaryColor,
+                                        inactiveColor:
+                                            Theme.of(context).primaryColorDark,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+
+                            //  next and previous icons
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  IconButton(onPressed: (){}, icon: const Icon(Icons.skip_previous_rounded)),
+                                  IconButton(onPressed: (){}, icon: const Icon(Icons.skip_next_rounded)),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ],
                     )
                   : const SizedBox.shrink(),
@@ -70,7 +154,8 @@ class _HomeBottomBarState extends State<HomeBottomBar> {
                   bottomBarItem(title: "Home", icon: Icons.home),
                   bottomBarItem(
                       title: "Playlist", icon: Icons.playlist_play_rounded),
-                  bottomBarItem(title: "Genres", icon: Icons.music_note_outlined),
+                  bottomBarItem(
+                      title: "Genres", icon: Icons.music_note_outlined),
                   bottomBarItem(title: "Settings", icon: Icons.settings),
                 ],
               )
