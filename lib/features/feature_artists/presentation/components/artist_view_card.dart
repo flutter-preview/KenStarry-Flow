069cfbb38f0f0a1114fdeae01e680e69df28307c
KenStarry <starrycodes@gmail.com>
2023-06-19 09:56:12 +0300

    Added Artist Page View

---
+++ lib/features/feature_artists/presentation/components/artist_view_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+
+class ArtistViewCard extends StatelessWidget {
+
+  final ArtistModel artistModel;
+
+  const ArtistViewCard({super.key, required this.artistModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        //  Artist image
+        ClipRRect(
+          borderRadius: BorderRadius.circular(300),
+          child: QueryArtworkWidget(
+            id: artistModel.id,
+            type: ArtworkType.ARTIST,
+            artworkFit: BoxFit.cover,
+            artworkWidth: 250,
+            artworkHeight: 250,
+            artworkQuality: FilterQuality.high,
+            keepOldArtwork: true,
+            quality: 100,
+            nullArtworkWidget: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColorDark,
+                    borderRadius: BorderRadius.circular(100)),
+                child: Icon(
+                  Icons.broken_image_rounded,
+                  color: Theme.of(context).iconTheme.color,
+                  size: 48,
+                )),
+          ),
+        ),
+
+        const SizedBox(height: 24,),
+        //  Artist details
+        Text(
+          artistModel.artist,
+          style: Theme.of(context).textTheme.bodyMedium,
+        ),
+      ],
+    );
+  }
+}
--- lib/features/feature_artists/presentation/screens/artist_view_screen.dart
+++ lib/features/feature_artists/presentation/screens/artist_view_screen.dart
@@ -1,8 +1,10 @@
+import 'package:flow/features/feature_artists/presentation/components/artist_view_card.dart';
+import 'package:flow/features/feature_artists/presentation/controller/artists_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:on_audio_query/on_audio_query.dart';
+import 'package:get/get.dart';
 
 class ArtistViewScreen extends StatefulWidget {
-
   final ArtistModel artist;
 
   const ArtistViewScreen({super.key, required this.artist});
@@ -12,14 +14,53 @@ class ArtistViewScreen extends StatefulWidget {
 }
 
 class _ArtistViewScreenState extends State<ArtistViewScreen> {
+  late final ArtistsController _artistsController;
+  late PageController _pageController;
 
   @override
   void initState() {
     super.initState();
+
+    _artistsController = Get.find();
+
+    //  scroll to the index where the artist has been clicked
+    _pageController = PageController(
+        viewportFraction: 0.8,
+        initialPage: _artistsController.artists
+            .indexWhere((a) => a.artist == widget.artist.artist));
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            //  Page View
+            Obx(
+              () => Container(
+                height: 350,
+                color: Colors.blue,
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: PageView.builder(
+                    controller: _pageController,
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: _artistsController.artists.length,
+                    itemBuilder: (context, index) {
+                      return ArtistViewCard(
+                          artistModel: _artistsController.artists[index]);
+                    }),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
