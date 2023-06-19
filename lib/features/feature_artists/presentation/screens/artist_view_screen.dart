94519eff1a046de98bc9426d4b2f6fd8d083e78a
KenStarry <starrycodes@gmail.com>
2023-06-19 10:06:42 +0300

    artist view page UI redesign

--- lib/features/feature_artists/presentation/screens/artist_view_screen.dart
+++ lib/features/feature_artists/presentation/screens/artist_view_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flow/features/feature_artists/presentation/components/artist_view_card.dart';
 import 'package:flow/features/feature_artists/presentation/controller/artists_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:get/get.dart';
+
+import '../../../../core/presentation/controller/core_controller.dart';
 
 class ArtistViewScreen extends StatefulWidget {
   final ArtistModel artist;
@@ -15,6 +18,7 @@ class ArtistViewScreen extends StatefulWidget {
 
 class _ArtistViewScreenState extends State<ArtistViewScreen> {
   late final ArtistsController _artistsController;
+  late final CoreController _coreController;
   late PageController _pageController;
 
   @override
@@ -22,6 +26,7 @@ class _ArtistViewScreenState extends State<ArtistViewScreen> {
     super.initState();
 
     _artistsController = Get.find();
+    _coreController = Get.find<CoreController>();
 
     //  scroll to the index where the artist has been clicked
     _pageController = PageController(
@@ -39,6 +44,17 @@ class _ArtistViewScreenState extends State<ArtistViewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColorDark,
+      appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Theme.of(context).primaryColorDark,
+          statusBarIconBrightness: _coreController.brightness.value == Brightness.dark
+              ? Brightness.light
+              : Brightness.dark
+        ),
+        backgroundColor: Theme.of(context).primaryColorDark,
+        elevation: 0,
+      ),
       body: SafeArea(
         child: Column(
           children: [
@@ -55,6 +71,13 @@ class _ArtistViewScreenState extends State<ArtistViewScreen> {
                       return ArtistViewCard(
                           artistModel: _artistsController.artists[index]);
                     }),
+              ),
+            ),
+
+            //  songs
+            Expanded(
+              child: Container(
+                color: Theme.of(context).scaffoldBackgroundColor,
               ),
             )
           ],
