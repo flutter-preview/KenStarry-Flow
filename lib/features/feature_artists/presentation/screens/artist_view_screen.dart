a6b58ca7557be0f0de36d1908c01bb90091f4be7
KenStarry <starrycodes@gmail.com>
2023-06-19 11:45:59 +0300

    Added songlist for every artist

--- lib/features/feature_artists/presentation/screens/artist_view_screen.dart
+++ lib/features/feature_artists/presentation/screens/artist_view_screen.dart
@@ -2,12 +2,16 @@ import 'package:flow/features/feature_artists/presentation/components/artist_vie
 import 'package:flow/features/feature_artists/presentation/components/artist_view_card.dart';
 import 'package:flow/features/feature_artists/presentation/controller/artists_controller.dart';
 import 'package:flow/core/presentation/controller/player_controller.dart';
+import 'package:flow/features/feature_home/presentation/controller/home_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:get/get.dart';
 
+import '../../../../core/presentation/components/show_player_bottom_sheet.dart';
 import '../../../../core/presentation/controller/core_controller.dart';
+import '../../../feature_home/domain/model/player_states.dart';
+import '../../../feature_home/presentation/components/song_card.dart';
 
 class ArtistViewScreen extends StatefulWidget {
   final ArtistModel artist;
@@ -22,6 +26,7 @@ class _ArtistViewScreenState extends State<ArtistViewScreen> {
   late final ArtistsController _artistsController;
   late final CoreController _coreController;
   late final PlayerController _playerController;
+  late final HomeController _homeController;
   late PageController _pageController;
 
   @override
@@ -31,6 +36,7 @@ class _ArtistViewScreenState extends State<ArtistViewScreen> {
     _artistsController = Get.find<ArtistsController>();
     _coreController = Get.find<CoreController>();
     _playerController = Get.find<PlayerController>();
+    _homeController = Get.find<HomeController>();
 
     //  scroll to the index where the artist has been clicked
     _pageController = PageController(
@@ -145,6 +151,43 @@ class _ArtistViewScreenState extends State<ArtistViewScreen> {
                           ),
                         ),
                       ],
+                    ),
+
+                    //  songs list
+                    Obx(
+                      () => Expanded(
+                        child: ListView.builder(
+                            physics: const BouncingScrollPhysics(),
+                            itemCount: _artistsController.artistSongs.length,
+                            itemBuilder: (context, index) => SongCard(
+                                  song: _artistsController.artistSongs[index],
+                                  songIndex: index,
+                                  coreController: _coreController,
+                                  playerController: _playerController,
+                                  onSongTapped: () {
+                                    if (_playerController.playerState.value ==
+                                            PlayerStates.playing &&
+                                        _playerController
+                                                .currentPlayingSongIndex.value ==
+                                            index) {
+                                      //  open player screen bottom sheet
+                                      showPlayerBottomSheet(
+                                          playerController: _playerController,
+                                          homeController: _homeController);
+                                    } else {
+                                      _playerController.playSong(
+                                          path: _artistsController
+                                              .artistSongs[index].uri!,
+                                          index: index);
+
+                                      //  open player screen bottom sheet
+                                      showPlayerBottomSheet(
+                                          playerController: _playerController,
+                                          homeController: _homeController);
+                                    }
+                                  },
+                                )),
+                      ),
                     )
                   ],
                 ),
