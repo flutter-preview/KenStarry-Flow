2d98dec0570856370a53fb0ded4c100d8e713167
KenStarry <starrycodes@gmail.com>
2023-06-19 21:52:47 +0300

    Added Hive to main screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,19 @@ import 'package:flow/features/feature_main/presentation/main_screen.dart';
 import 'package:flow/theme/my_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:hive/hive.dart';
 
 Future<void> main() async {
 
   WidgetsFlutterBinding.ensureInitialized();
+
+  final appDocumentDirectory =
+  await path_provider.getApplicationDocumentsDirectory();
+
+  await Hive.initFlutter(appDocumentDirectory.path);
+
   await invokeDI();
 
   runApp(const MyApp());
