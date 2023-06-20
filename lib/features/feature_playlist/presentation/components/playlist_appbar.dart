d5820cf281872dccadc78f429e476c427d44af1f
KenStarry <starrycodes@gmail.com>
2023-06-20 11:11:30 +0300

    Added Sort icon in playlist page

--- lib/features/feature_playlist/presentation/components/playlist_appbar.dart
+++ lib/features/feature_playlist/presentation/components/playlist_appbar.dart
@@ -5,23 +5,29 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 SliverAppBar playlistAppBar() {
-
   final CoreController controller = Get.find();
 
   return SliverAppBar(
     systemOverlayStyle: SystemUiOverlayStyle(
         statusBarColor: Theme.of(Get.context!).scaffoldBackgroundColor,
-        statusBarIconBrightness:
-        controller.brightness.value == Brightness.dark
+        statusBarIconBrightness: controller.brightness.value == Brightness.dark
             ? Brightness.light
             : Brightness.dark),
+    actions: [
+      IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.sort_rounded,
+            color: Theme.of(Get.context!).iconTheme.color,
+          ))
+    ],
     expandedHeight: 100,
     floating: false,
     pinned: true,
     backgroundColor: Theme.of(Get.context!).scaffoldBackgroundColor,
     flexibleSpace: FlexibleSpaceBar(
       title: Text(
-        "Playlist",
+        "Playlists",
         style: Theme.of(Get.context!).textTheme.titleSmall,
       ),
       centerTitle: true,
