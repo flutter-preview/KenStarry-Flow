dd650f4363422ae2b872d6130dcb7494f3a874c0
KenStarry <starrycodes@gmail.com>
2023-06-20 11:21:07 +0300

    New Playlist button

---
+++ lib/features/feature_playlist/presentation/components/new_playlist_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+Widget addNewPlaylistBtn({required VoidCallback onTap}) =>
+    GestureDetector(onTap: onTap, child: Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        color: Theme.of(Get.context!).primaryColorDark
+      ),
+      child: Row(
+        children: [
+          Icon(Icons.add, color: Theme.of(Get.context!).iconTheme.color,),
+          const SizedBox(width: 8,),
+          Text("Add playlist", style: Theme.of(Get.context!).textTheme.bodySmall,)
+        ],
+      ),
+    ));
--- lib/features/feature_playlist/presentation/playlist_screen.dart
+++ lib/features/feature_playlist/presentation/playlist_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flow/features/feature_playlist/presentation/components/playlist_appbar.dart';
 import 'package:flutter/material.dart';
 
+import 'components/new_playlist_button.dart';
+
 class PlaylistScreen extends StatefulWidget {
   const PlaylistScreen({super.key});
 
@@ -15,7 +17,19 @@ class _PlaylistScreenState extends State<PlaylistScreen> {
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: CustomScrollView(
         slivers: [
-          playlistAppBar()
+          playlistAppBar(),
+          //  header
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  //  add new playlist
+                  addNewPlaylistBtn(onTap: () {})
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
