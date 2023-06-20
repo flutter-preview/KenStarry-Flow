4974f98b178bb79e5383fb792c575f6e20f091fa
KenStarry <starrycodes@gmail.com>
2023-06-20 16:41:50 +0300

    Modified playlist repo

--- lib/features/feature_playlist/domain/repository/playlist_repository.dart
+++ lib/features/feature_playlist/domain/repository/playlist_repository.dart
@@ -1,7 +1,9 @@
 abstract class PlaylistRepository {
 
-  /// Create Playlist
-  /// Delete Playlist
+  /// Add Playlist
+  Future<void> addPlaylist();
   /// Update Playlist
-
+  Future<void> updatePlaylist();
+  /// Delete Playlist
+  Future<void> deletePlaylist();
 }
