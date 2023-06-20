28decef8a153c424a5289727ae0b96f9ad6a468d
KenStarry <starrycodes@gmail.com>
2023-06-20 09:33:43 +0300

    Added Hive Box

--- lib/core/data/repository/core_repository_impl.dart
+++ lib/core/data/repository/core_repository_impl.dart
@@ -1,10 +1,12 @@
 import 'package:audio_service/audio_service.dart';
 import 'package:flow/core/data/source/my_audio_handler.dart';
 import 'package:flow/core/domain/repository/core_repository.dart';
+import 'package:flow/core/utils/hive_utils.dart';
+import 'package:hive/hive.dart';
 
 class CoreRepositoryImpl implements CoreRepository {
 
-
+  final Box userBox = Hive.box(HiveUtils.userBox);
 
   @override
   Future<void> addUserPrefs() {
