1774eb484371082ecbafc3c6ad113700a1487939
KenStarry <starrycodes@gmail.com>
2023-06-20 09:31:09 +0300

    Added user prefs in Core Repository

--- lib/core/domain/repository/core_repository.dart
+++ lib/core/domain/repository/core_repository.dart
@@ -1,5 +1,15 @@
 import 'package:audio_service/audio_service.dart';
 
 abstract class CoreRepository {
-  /// Audio Handler
+  /// Add User Preferences
+  Future<void> addUserPrefs();
+
+  /// Get User Preferences
+  Future<void> getUserPrefs();
+
+  /// Update User Preferences
+  Future<void> updateUserPrefs();
+
+  /// Delete User Preferences
+  Future<void> deleteUserPrefs();
 }
