cf491d266265a1ecb7219a395b9ff70628272b7b
KenStarry <starrycodes@gmail.com>
2023-06-20 09:32:24 +0300

    Implementations for the repository

--- lib/core/data/repository/core_repository_impl.dart
+++ lib/core/data/repository/core_repository_impl.dart
@@ -3,4 +3,30 @@ import 'package:flow/core/data/source/my_audio_handler.dart';
 import 'package:flow/core/domain/repository/core_repository.dart';
 
 class CoreRepositoryImpl implements CoreRepository {
+
+
+
+  @override
+  Future<void> addUserPrefs() {
+    // TODO: implement addUserPrefs
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteUserPrefs() {
+    // TODO: implement deleteUserPrefs
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> getUserPrefs() {
+    // TODO: implement getUserPrefs
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> updateUserPrefs() {
+    // TODO: implement updateUserPrefs
+    throw UnimplementedError();
+  }
 }
