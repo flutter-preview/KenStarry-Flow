d89eb5ddc7c14b58361184c3f5e7ed548fd4a1d4
KenStarry <starrycodes@gmail.com>
2023-06-20 09:52:22 +0300

    Added use cases to DI

--- lib/di/hive_di.dart
+++ lib/di/hive_di.dart
@@ -1,9 +1,20 @@
 import 'package:flow/core/data/repository/hive_repository_impl.dart';
 import 'package:flow/core/domain/repository/hive_repository.dart';
+import 'package:flow/core/domain/use_cases/hive_repo/add_user_prefs.dart';
+import 'package:flow/core/domain/use_cases/hive_repo/delete_user_prefs.dart';
+import 'package:flow/core/domain/use_cases/hive_repo/get_user_prefs.dart';
+import 'package:flow/core/domain/use_cases/hive_repo/hive_use_cases.dart';
+import 'package:flow/core/domain/use_cases/hive_repo/update_user_prefs.dart';
 import 'package:get_it/get_it.dart';
 
 void hiveDI({required GetIt locator}) {
-
   /// Provide Repository
   locator.registerLazySingleton<HiveRepository>(() => HiveRepositoryImpl());
+
+  /// Provide Use Cases
+  locator.registerLazySingleton<HiveUseCases>(() => HiveUseCases(
+      addUserPrefs: AddUserPrefs(),
+      getUserPrefs: GetUserPrefs(),
+      deleteUserPrefs: DeleteUserPrefs(),
+      updateUserPrefs: UpdateUserPrefs()));
 }
