8a8f177f6a73d1c80bfea273b72396bf82bf0a76
KenStarry <starrycodes@gmail.com>
2023-06-21 15:36:02 +0300

    Modified Playlist card

--- lib/features/feature_playlist/presentation/components/playlist_card.dart
+++ lib/features/feature_playlist/presentation/components/playlist_card.dart
@@ -18,25 +18,37 @@ class PlaylistCard extends StatelessWidget {
               width: 150,
               height: 150,
               decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColorDark,
+                  color: Theme
+                      .of(context)
+                      .primaryColorDark,
                   borderRadius: BorderRadius.circular(24)),
-              child: Icon(Icons.music_note_outlined, color: Theme.of(context).iconTheme.color, size: 48,),
+              child: Icon(Icons.music_note_outlined, color: Theme
+                  .of(context)
+                  .iconTheme
+                  .color, size: 48,),
             ),
             //  songs count
             Align(
               alignment: AlignmentDirectional.centerEnd,
               child: Container(
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
+                const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
                 decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColorDark,
+                    color: Theme
+                        .of(context)
+                        .primaryColorDark,
                     borderRadius: BorderRadius.circular(30),
                     border: Border.all(
-                        color: Theme.of(context).scaffoldBackgroundColor,
+                        color: Theme
+                            .of(context)
+                            .scaffoldBackgroundColor,
                         width: 5)),
                 child: Text(
                   "30",
-                  style: Theme.of(context).textTheme.bodySmall,
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .bodySmall,
                 ),
               ),
             )
@@ -44,26 +56,43 @@ class PlaylistCard extends StatelessWidget {
         ),
 
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          padding: const EdgeInsets.only(left: 8.0),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               //  playlist name
               Text(
                 playlist.playlistName ?? "No name",
-                style: Theme.of(context).textTheme.bodyMedium,
+                style: Theme
+                    .of(context)
+                    .textTheme
+                    .bodyMedium,
               ),
               //  playlist more action
               PopupMenuButton(
-                  itemBuilder: (context) => [
-                        PopupMenuItem(
-                          child: Text(
+                  itemBuilder: (context) =>
+                  [
+                    PopupMenuItem(
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.delete_outline_rounded, size: 16, color: Theme
+                              .of(context)
+                              .iconTheme
+                              .color,),
+                          const SizedBox(width: 8,),
+                          Text(
                             "Delete",
-                            style: Theme.of(context).textTheme.bodyMedium,
-                          ),
-                          onTap: (){},
-                        ),
-                      ])
+                            style: Theme
+                                .of(context)
+                                .textTheme
+                                .bodyMedium,
+                          )
+                        ],
+                      ),
+                      onTap: () {},
+                    ),
+                  ])
             ],
           ),
         )
