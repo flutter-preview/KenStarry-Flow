4becf221ebc231d1f02fbfb46dd44084796e1e22
KenStarry <starrycodes@gmail.com>
2023-06-21 10:11:09 +0300

    Show modal bottom sheet content

--- lib/features/feature_playlist/presentation/components/bottomsheets/create_playlist_bottom_sheet.dart
+++ lib/features/feature_playlist/presentation/components/bottomsheets/create_playlist_bottom_sheet.dart
@@ -1,1 +1,14 @@
+import 'package:flow/features/feature_playlist/presentation/components/bottomsheets/playlist_bottomsheet_content.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+Future createPlaylistBottomSheet() => showModalBottomSheet(
+  isDismissible: true,
+    isScrollControlled: true,
+    showDragHandle: true,
+    backgroundColor: Theme.of(Get.context!).scaffoldBackgroundColor,
+    shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(24), topRight: Radius.circular(24))),
+    context: Get.context!,
+    builder: (context) => const PlaylistBottomSheetContent());
