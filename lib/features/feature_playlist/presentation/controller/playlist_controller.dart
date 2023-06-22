61dad370d3038a52f934fa427fcb759f4cfcafcf
KenStarry <starrycodes@gmail.com>
2023-06-22 10:52:33 +0300

    Added Playlist Controller to get playlist

--- lib/features/feature_playlist/presentation/controller/playlist_controller.dart
+++ lib/features/feature_playlist/presentation/controller/playlist_controller.dart
@@ -1,7 +1,6 @@
 import 'package:flow/core/utils/hive_utils.dart';
 import 'package:flow/di/locator.dart';
 import 'package:flow/features/feature_playlist/domain/use_case/playlist_use_cases.dart';
-import 'package:get_it/get_it.dart';
 import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
@@ -38,4 +37,7 @@ class PlaylistController extends GetxController {
   Future<void> updatePlaylist(
           {required int index, required Playlist playlist}) async =>
       await useCases.updatePlaylist(index: index, playlist: playlist);
+
+  Future<Playlist> getPlaylist({required int index}) async =>
+      await useCases.getPlaylist.call(index: index);
 }
