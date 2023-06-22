87169dcf25797550f1c95740d44362b85e4fe8ab
KenStarry <starrycodes@gmail.com>
2023-06-22 14:11:47 +0300

    Added carousel card

---
+++ lib/features/feature_playlist/presentation/components/view_playlist_carousel_card.dart
@@ -0,0 +1,15 @@
+import 'package:flow/features/feature_playlist/domain/model/playlist.dart';
+import 'package:flutter/material.dart';
+
+class ViewPlaylistCarouselCard extends StatelessWidget {
+  final Playlist playlist;
+
+  const ViewPlaylistCarouselCard({super.key, required this.playlist});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
--- lib/features/feature_playlist/presentation/screens/view_playlist_screen.dart
+++ lib/features/feature_playlist/presentation/screens/view_playlist_screen.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flow/features/feature_playlist/domain/model/playlist.dart';
+import 'package:flow/features/feature_playlist/presentation/components/view_playlist_carousel_card.dart';
 import 'package:flutter/material.dart';
 
 import '../controller/playlist_controller.dart';
@@ -24,13 +26,32 @@ class _ViewPlaylistScreenState extends State<ViewPlaylistScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading:
-            IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back)),
-        elevation: 0,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      ),
-      body: Text("Hello"),
-    );
+        appBar: AppBar(
+          leading:
+              IconButton(onPressed: () {}, icon: const Icon(Icons.arrow_back)),
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        ),
+        body: ValueListenableBuilder(
+            valueListenable: _playlistController.playlistsBox.value,
+            builder: (context, box, widget) {
+              final List<Playlist>? playlists =
+                  box.values.cast<Playlist>().toList();
+
+              return Column(
+                children: [
+                  //  carousel playlist slider
+                  CarouselSlider.builder(
+                      itemCount: playlists?.length ?? 0,
+                      itemBuilder: (context, index, realIndex) {
+                        final currentPlaylist = playlists?[index];
+                        return ViewPlaylistCarouselCard(
+                            playlist: currentPlaylist!);
+                      },
+                      options:
+                          CarouselOptions(height: 300))
+                ],
+              );
+            }));
   }
 }
