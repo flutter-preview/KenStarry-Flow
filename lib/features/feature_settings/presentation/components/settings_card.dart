3b0ace584bfd5490ef6972881e3b758d10dac333
KenStarry <starrycodes@gmail.com>
2023-06-23 21:49:49 +0300

    Added Container for settings card

--- lib/features/feature_settings/presentation/components/settings_card.dart
+++ lib/features/feature_settings/presentation/components/settings_card.dart
@@ -4,11 +4,26 @@ class SettingsCard extends StatelessWidget {
 
   final String title;
   final IconData iconData;
+  final Color color;
 
-  const SettingsCard({super.key, required this.title, required this.iconData});
+  const SettingsCard({super.key, required this.title, required this.iconData, required this.color});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.red,
+        borderRadius: BorderRadius.circular(16)
+      ),
+      child: Row(
+        children: [
+          //  icon
+
+          //  title
+        ],
+      ),
+    );
   }
 }
