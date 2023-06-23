6471c8999c9d3aca99a1d3a540fcb9824053964c
KenStarry <starrycodes@gmail.com>
2023-06-23 22:09:34 +0300

    Settings list

--- lib/features/feature_settings/presentation/utils/settings_constants.dart
+++ lib/features/feature_settings/presentation/utils/settings_constants.dart
@@ -1,3 +1,10 @@
+import 'package:flow/features/feature_settings/presentation/components/settings_card.dart';
+import 'package:flutter/material.dart';
+
 class SettingsConstants {
 
+  static const settings = [
+    SettingsCard(title: "Theme", iconData: Icons.dark_mode_rounded, color: Colors.orangeAccent),
+    SettingsCard(title: "Player", iconData: Icons.play_arrow_rounded, color: Colors.greenAccent),
+  ];
 }
