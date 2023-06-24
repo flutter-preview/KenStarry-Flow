20ae9a44ef1f19e9106783e6098d13318835b626
KenStarry <starrycodes@gmail.com>
2023-06-24 15:02:58 +0300

    Added Phone Camera

--- lib/features/feature_settings/presentation/components/setting_types/theme_settings/theme_mockup.dart
+++ lib/features/feature_settings/presentation/components/setting_types/theme_settings/theme_mockup.dart
@@ -14,12 +14,37 @@ class _ThemeMockupState extends State<ThemeMockup> {
     return Align(
       alignment: AlignmentDirectional.center,
       child: Container(
-        width: MediaQuery.of(context).size.width * 0.6,
+        width: MediaQuery.of(context).size.width * 0.55,
         height: 300,
+        padding: const EdgeInsets.all(8),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(24),
           color: Theme.of(context).scaffoldBackgroundColor,
           border: Border.all(color: Colors.black.lightenColor(90), width: 3)
+        ),
+        child: Stack(
+          fit: StackFit.loose,
+          children: [
+            //  phone camera
+            Container(
+              width: 20,
+              height: 20,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: Colors.black.lightenColor(50),
+              ),
+              child: Center(
+                child: Container(
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
