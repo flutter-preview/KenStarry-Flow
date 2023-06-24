acf6e4d5ecf03dccd7c23be5cfc4f20f9eb63063
KenStarry <starrycodes@gmail.com>
2023-06-24 12:33:38 +0300

    Setting type enum and view setting screen

---
+++ lib/features/feature_settings/domain/model/setting_type.dart
@@ -0,0 +1,1 @@
+enum SettingType { theme, player }
---
+++ lib/features/feature_settings/presentation/screens/view_setting_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ViewSettingScreen extends StatefulWidget {
+
+
+
+  const ViewSettingScreen({super.key});
+
+  @override
+  State<ViewSettingScreen> createState() => _ViewSettingScreenState();
+}
+
+class _ViewSettingScreenState extends State<ViewSettingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
