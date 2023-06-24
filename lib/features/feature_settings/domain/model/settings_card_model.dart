b56dcf42d6bb4718e42d373014a66d6d91db462e
KenStarry <starrycodes@gmail.com>
2023-06-24 12:24:49 +0300

    Added Settings card mode

---
+++ lib/features/feature_settings/domain/model/settings_card_model.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class SettingsCardModel {
+  final String title;
+  final IconData iconData;
+  final Color color;
+
+  SettingsCardModel(
+      {required this.title, required this.iconData, required this.color});
+}
