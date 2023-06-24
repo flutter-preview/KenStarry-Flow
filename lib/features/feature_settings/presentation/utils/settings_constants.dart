3a6a410e70737ed5b529da45fe9983bfddcda789
KenStarry <starrycodes@gmail.com>
2023-06-24 12:26:23 +0300

    Changed settings to use card model

--- lib/features/feature_settings/presentation/utils/settings_constants.dart
+++ lib/features/feature_settings/presentation/utils/settings_constants.dart
@@ -1,10 +1,11 @@
+import 'package:flow/features/feature_settings/domain/model/settings_card_model.dart';
 import 'package:flow/features/feature_settings/presentation/components/settings_card.dart';
 import 'package:flutter/material.dart';
 
 class SettingsConstants {
 
-  static const settings = [
-    SettingsCard(title: "Theme", iconData: Icons.dark_mode_rounded, color: Colors.orangeAccent),
-    SettingsCard(title: "Player", iconData: Icons.play_arrow_rounded, color: Colors.greenAccent),
+  static var settings = <SettingsCardModel>[
+    SettingsCardModel(title: "Theme", iconData: Icons.dark_mode_rounded, color: Colors.orangeAccent),
+    SettingsCardModel(title: "Player", iconData: Icons.play_arrow_rounded, color: Colors.greenAccent),
   ];
 }
