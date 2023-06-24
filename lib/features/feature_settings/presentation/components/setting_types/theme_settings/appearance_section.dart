fe585169bb77d0ef0defbd944b35619c8194c387
KenStarry <starrycodes@gmail.com>
2023-06-24 18:48:16 +0300

    Converted Appearance section to stateful widget

--- lib/features/feature_settings/presentation/components/setting_types/theme_settings/appearance_section.dart
+++ lib/features/feature_settings/presentation/components/setting_types/theme_settings/appearance_section.dart
@@ -1,9 +1,14 @@
 import 'package:flow/features/feature_settings/presentation/components/setting_types/theme_settings/light_theme_mockup.dart';
 import 'package:flutter/material.dart';
 
-class AppearanceSection extends StatelessWidget {
+class AppearanceSection extends StatefulWidget {
   const AppearanceSection({super.key});
 
+  @override
+  State<AppearanceSection> createState() => _AppearanceSectionState();
+}
+
+class _AppearanceSectionState extends State<AppearanceSection> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -11,6 +16,7 @@ class AppearanceSection extends StatelessWidget {
       children: [
         Text("Appearance", style: Theme.of(context).textTheme.titleSmall,),
         const SizedBox(height: 16,),
+        //
         LightThemeMockup()
       ],
     );
