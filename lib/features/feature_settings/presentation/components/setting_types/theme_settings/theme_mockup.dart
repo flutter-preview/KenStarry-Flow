07f9d4d5f8be2c6f6cdea5b4c9f8b5689835b3c2
KenStarry <starrycodes@gmail.com>
2023-06-24 14:50:47 +0300

    Theme Mockup stateful

--- lib/features/feature_settings/presentation/components/setting_types/theme_settings/theme_mockup.dart
+++ lib/features/feature_settings/presentation/components/setting_types/theme_settings/theme_mockup.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class ThemeMockup extends StatelessWidget {
+class ThemeMockup extends StatefulWidget {
   const ThemeMockup({super.key});
 
+  @override
+  State<ThemeMockup> createState() => _ThemeMockupState();
+}
+
+class _ThemeMockupState extends State<ThemeMockup> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
