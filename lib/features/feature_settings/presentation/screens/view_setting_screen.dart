ac8ea7844ed3d46aad29f422764a9f05f5161073
KenStarry <starrycodes@gmail.com>
2023-06-24 12:52:10 +0300

    Added App bar implementation for the different themes

--- lib/features/feature_settings/presentation/screens/view_setting_screen.dart
+++ lib/features/feature_settings/presentation/screens/view_setting_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flow/features/feature_settings/domain/model/setting_type.dart';
+import 'package:flow/features/feature_settings/presentation/components/setting_types/player_settings.dart';
 import 'package:flow/features/feature_settings/presentation/components/setting_types/theme_settings.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ViewSettingScreen extends StatefulWidget {
   final SettingType type;
@@ -17,12 +19,35 @@ class _ViewSettingScreenState extends State<ViewSettingScreen> {
       case SettingType.theme:
         return ThemeSettings();
       case SettingType.player:
-        return ThemeSettings();
+        return PlayerSettings();
+    }
+  }
+
+  String getTitle({required SettingType type}) {
+    switch (type) {
+      case SettingType.theme:
+        return "Theme";
+      case SettingType.player:
+        return "Player";
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          getTitle(type: widget.type),
+          style: Theme.of(context).textTheme.titleSmall,
+        ),
+        elevation: 0,
+        leading: IconButton(
+            onPressed: () => Get.back(canPop: true),
+            icon: Icon(
+              Icons.arrow_back,
+              color: Theme.of(context).iconTheme.color,
+            )),
+      ),
+    );
   }
 }
