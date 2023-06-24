272a85d6a866a4c0bd672607fc616ff82b1cdfe3
KenStarry <starrycodes@gmail.com>
2023-06-24 12:57:04 +0300

    Centered appbar title and changed settings status bar color

--- lib/features/feature_settings/presentation/screens/view_setting_screen.dart
+++ lib/features/feature_settings/presentation/screens/view_setting_screen.dart
@@ -2,7 +2,10 @@ import 'package:flow/features/feature_settings/domain/model/setting_type.dart';
 import 'package:flow/features/feature_settings/presentation/components/setting_types/player_settings.dart';
 import 'package:flow/features/feature_settings/presentation/components/setting_types/theme_settings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+
+import '../../../../core/presentation/controller/core_controller.dart';
 
 class ViewSettingScreen extends StatefulWidget {
   final SettingType type;
@@ -14,6 +17,15 @@ class ViewSettingScreen extends StatefulWidget {
 }
 
 class _ViewSettingScreenState extends State<ViewSettingScreen> {
+  late final CoreController _coreController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _coreController = Get.find<CoreController>();
+  }
+
   Widget getScreen({required SettingType type}) {
     switch (type) {
       case SettingType.theme:
@@ -36,10 +48,18 @@ class _ViewSettingScreenState extends State<ViewSettingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: Theme.of(context).scaffoldBackgroundColor,
+          statusBarIconBrightness:
+          _coreController.brightness.value == Brightness.dark
+              ? Brightness.light
+              : Brightness.dark,
+        ),
         title: Text(
           getTitle(type: widget.type),
           style: Theme.of(context).textTheme.titleSmall,
         ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         elevation: 0,
         leading: IconButton(
             onPressed: () => Get.back(canPop: true),
@@ -47,6 +67,7 @@ class _ViewSettingScreenState extends State<ViewSettingScreen> {
               Icons.arrow_back,
               color: Theme.of(context).iconTheme.color,
             )),
+        centerTitle: true,
       ),
     );
   }
