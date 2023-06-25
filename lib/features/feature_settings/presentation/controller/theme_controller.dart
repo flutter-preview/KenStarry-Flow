b5479188dfb4cdfcbe49325b2d9f34b82f722080
KenStarry <starrycodes@gmail.com>
2023-06-25 14:07:12 +0300

    Added Selected Hex color value to theme controller

--- lib/features/feature_settings/presentation/controller/theme_controller.dart
+++ lib/features/feature_settings/presentation/controller/theme_controller.dart
@@ -2,16 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ThemeController extends GetxController {
-
   /// Theme Setting
   final selectedThemeIndex = 0.obs;
   final selectedThemeMode = ThemeMode.system.obs;
+  final selectedAccentColorHexValue = ''.obs;
 
   final themeTypes = const ['Light Theme', 'Dark Theme', 'System Preferences'];
 
   final themeModes = const [ThemeMode.light, ThemeMode.dark, ThemeMode.system];
 
-  void setSelectedThemeIndex({required int index}) => selectedThemeIndex.value = index;
+  void setSelectedThemeIndex({required int index}) =>
+      selectedThemeIndex.value = index;
 
-  ThemeMode setThemeMode({required ThemeMode mode}) => selectedThemeMode.value = mode;
+  ThemeMode setThemeMode({required ThemeMode mode}) =>
+      selectedThemeMode.value = mode;
+
+  void setSelectedAccentColorHexValue({required String hex}) =>
+      selectedAccentColorHexValue.value = hex;
 }
