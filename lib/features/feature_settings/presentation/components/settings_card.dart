c0ef5fc215dc9b1751fe35cd862b1bd1f547e86d
KenStarry <starrycodes@gmail.com>
2023-06-25 20:48:47 +0300

    Made settings icons rounded

--- lib/features/feature_settings/presentation/components/settings_card.dart
+++ lib/features/feature_settings/presentation/components/settings_card.dart
@@ -1,21 +1,23 @@
+import 'package:flow/core/utils/extensions/string_extensions.dart';
 import 'package:flow/features/feature_settings/domain/model/settings_card_model.dart';
 import 'package:flow/features/feature_settings/presentation/controller/theme_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SettingsCard extends StatefulWidget {
-
   final SettingsCardModel settingsCardModel;
   final VoidCallback onCardClicked;
 
-  const SettingsCard({super.key, required this.settingsCardModel, required this.onCardClicked});
+  const SettingsCard(
+      {super.key,
+      required this.settingsCardModel,
+      required this.onCardClicked});
 
   @override
   State<SettingsCard> createState() => _SettingsCardState();
 }
 
 class _SettingsCardState extends State<SettingsCard> {
-
   late final ThemeController _themeController;
 
   @override
@@ -33,17 +35,33 @@ class _SettingsCardState extends State<SettingsCard> {
         width: double.infinity,
         padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
         decoration: BoxDecoration(
-          color: Theme.of(context).scaffoldBackgroundColor,
-          borderRadius: BorderRadius.circular(16)
-        ),
+            color: Theme.of(context).scaffoldBackgroundColor,
+            borderRadius: BorderRadius.circular(16)),
         margin: const EdgeInsets.symmetric(vertical: 8),
         child: Row(
           children: [
             //  icon
-            Icon(widget.settingsCardModel.iconData, size: 24, color: widget.settingsCardModel.color,),
-            const SizedBox(width: 8,),
+            Container(
+              width: 50,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColorDark,
+                borderRadius: BorderRadius.circular(100)
+              ),
+              child: Icon(
+                widget.settingsCardModel.iconData,
+                size: 24,
+                color: _themeController.selectedAccentColorHex.value.toColor as Color,
+              ),
+            ),
+            const SizedBox(
+              width: 8,
+            ),
             //  title
-            Text(widget.settingsCardModel.title, style: Theme.of(context).textTheme.bodyLarge,)
+            Text(
+              widget.settingsCardModel.title,
+              style: Theme.of(context).textTheme.bodyLarge,
+            )
           ],
         ),
       ),
