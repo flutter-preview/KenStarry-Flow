86deb0f3e10912d19a366d3f21802f115b60b515
KenStarry <starrycodes@gmail.com>
2023-06-25 21:06:22 +0300

    Color Picker dialog widget

--- lib/features/feature_settings/presentation/utils/showPickColorDialog.dart
+++ lib/features/feature_settings/presentation/utils/showPickColorDialog.dart
@@ -1,5 +1,10 @@
+import 'package:flow/features/feature_settings/presentation/components/setting_types/theme_settings/color_picker_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void showPickColorDialog() =>
-    showDialog(context: Get.context!, builder: (context) => AlertDialog());
+    showDialog(context: Get.context!, builder: (context) => AlertDialog(
+      title: Text("Pick Accent Color"),
+      icon: Icon(Icons.color_lens_rounded, color: Theme.of(Get.context!).iconTheme.color),
+      content: const ColorPickerDialog(),
+    ));
