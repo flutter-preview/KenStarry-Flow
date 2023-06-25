24c873139b3d8ae60b35a9339fef97a6d5af58cc
KenStarry <starrycodes@gmail.com>
2023-06-25 13:55:13 +0300

    Added Accent Colors

--- lib/features/feature_settings/presentation/components/setting_types/theme_settings/accent_color_card.dart
+++ lib/features/feature_settings/presentation/components/setting_types/theme_settings/accent_color_card.dart
@@ -1,3 +1,4 @@
+import 'package:flow/core/utils/extensions/color_extensions.dart';
 import 'package:flutter/material.dart';
 
 class AccentColorCard extends StatelessWidget {
@@ -9,28 +10,37 @@ class AccentColorCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return UnconstrainedBox(
-      child: Stack(
-        fit: StackFit.loose,
-        children: [
-          Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-                color: color,
-                borderRadius: BorderRadius.circular(100)
+      child: Container(
+        width: 50,
+        height: 50,
+        child: Stack(
+          fit: StackFit.loose,
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              margin: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                  color: color,
+                  borderRadius: BorderRadius.circular(100)
+              ),
             ),
-          ),
-          //  tick
-          Container(
-            width: 20,
-            height: 20,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(100),
-              color: Theme.of(context).scaffoldBackgroundColor
-            ),
-            child: Icon(Icons.done_rounded, color: color, size: 16,),
-          )
-        ],
+            //  tick
+            Align(
+              alignment: AlignmentDirectional.topEnd,
+              child: Container(
+                width: 20,
+                height: 20,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                  color: color.lightenColor(92),
+                  border: Border.all(color: Theme.of(context).scaffoldBackgroundColor, width: 2)
+                ),
+                child: Icon(Icons.done_rounded, color: color, size: 16,),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
