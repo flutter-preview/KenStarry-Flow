6eeb1ef6aea126129f8df916db79a9513a93fb21
KenStarry <starrycodes@gmail.com>
2023-06-25 15:17:32 +0300

    Modified Save and discard buttons appearance

--- lib/features/feature_settings/presentation/components/setting_types/theme_settings/theme_settings.dart
+++ lib/features/feature_settings/presentation/components/setting_types/theme_settings/theme_settings.dart
@@ -1,10 +1,14 @@
 import 'package:flow/core/presentation/controller/user_controller.dart';
+import 'package:flow/core/utils/extensions/color_extensions.dart';
+import 'package:flow/core/utils/extensions/string_extensions.dart';
 import 'package:flow/features/feature_settings/presentation/components/setting_types/theme_settings/accent_section.dart';
 import 'package:flow/features/feature_settings/presentation/components/setting_types/theme_settings/appearance_section.dart';
 import 'package:flow/features/feature_settings/presentation/components/setting_types/theme_settings/theme_setting_appbar.dart';
 import 'package:flow/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../../controller/theme_controller.dart';
 
 class ThemeSettings extends StatefulWidget {
   const ThemeSettings({super.key});
@@ -14,6 +18,15 @@ class ThemeSettings extends StatefulWidget {
 }
 
 class _ThemeSettingsState extends State<ThemeSettings> {
+  late final ThemeController _themeController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _themeController = Get.find<ThemeController>();
+  }
+
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
@@ -27,8 +40,12 @@ class _ThemeSettingsState extends State<ThemeSettings> {
             sliver: SliverToBoxAdapter(child: AppearanceSection())),
 
         //  Accent Picker section
-        const SliverPadding(padding: EdgeInsets.symmetric(horizontal: 16),
-        sliver: SliverToBoxAdapter(child: AccentSection(),),),
+        const SliverPadding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          sliver: SliverToBoxAdapter(
+            child: AccentSection(),
+          ),
+        ),
 
         const SliverToBoxAdapter(child: SizedBox(height: 24)),
 
@@ -40,22 +57,45 @@ class _ThemeSettingsState extends State<ThemeSettings> {
               mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                TextButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                          Theme.of(context).scaffoldBackgroundColor),
-                      foregroundColor: MaterialStateProperty.all(accent),
-                    ),
-                    child: Text("Discard")),
+                Obx(
+                  () => TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                            Theme.of(context).scaffoldBackgroundColor),
+                        foregroundColor: MaterialStateProperty.all(
+                            (_themeController.selectedAccentColorHex.value
+                                .toColor as Color)),
+                      ),
+                      child: Text("Discard")),
+                ),
                 const SizedBox(
                   width: 16,
                 ),
-                FilledButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(accent)),
-                    child: Text("Save")),
+                Obx(
+                  () => FilledButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              (_themeController.selectedAccentColorHex.value
+                                  .toColor as Color))),
+                      child: Text(
+                        "Save",
+                        style: TextStyle(
+                            fontWeight: Theme.of(context)
+                                .textTheme
+                                .bodyMedium
+                                ?.fontWeight,
+                            fontSize: Theme.of(context)
+                                .textTheme
+                                .bodyMedium
+                                ?.fontSize,
+                            color: getTextColorForBackground((_themeController
+                                .selectedAccentColorHex
+                                .value
+                                .toColor as Color))),
+                      )),
+                ),
               ],
             ),
           ),
