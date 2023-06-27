5bd73cad7e981fac4df6eeb468b928feeb37fc4e
KenStarry <starrycodes@gmail.com>
2023-06-27 15:34:54 +0300

    Implemented Shuffle songs on Home Screen

--- lib/features/feature_home/presentation/home_screen.dart
+++ lib/features/feature_home/presentation/home_screen.dart
@@ -9,9 +9,13 @@ import 'package:flow/core/presentation/controller/player_controller.dart';
 import 'package:flow/features/feature_main/presentation/components/my_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 import 'package:get/get.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
+
+import '../../../core/presentation/components/show_toast.dart';
+import '../../../core/utils/extensions/color_extensions.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({
@@ -27,6 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
   late final CoreController _coreController;
   late final HomeController _homeController;
   late final ItemScrollController _scrollController;
+  late final FToast _toast;
 
   @override
   void initState() {
@@ -36,14 +41,15 @@ class _HomeScreenState extends State<HomeScreen> {
     _coreController = Get.find();
     _homeController = Get.find();
     _scrollController = ItemScrollController();
+    _toast = FToast();
+    _toast.init(context);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.only(left: 8.0,
-        top: 8),
+        padding: const EdgeInsets.only(left: 8.0, top: 8),
         child: CustomScrollView(
           physics: const BouncingScrollPhysics(),
           slivers: [
@@ -89,19 +95,45 @@ class _HomeScreenState extends State<HomeScreen> {
                     InkWell(
                       borderRadius: BorderRadius.circular(100),
                       onTap: () {
-                        //  show songs
+                        //  shuffle songs
+                        _playerController.shuffle();
+
+                        if (_playerController.isShuffleModeEnabled.value) {
+                          showToast(
+                              toast: _toast,
+                              iconData: Icons.shuffle_rounded,
+                              msg: "Shuffle on");
+                        } else {
+                          showToast(
+                              toast: _toast,
+                              iconData: Icons.shuffle_rounded,
+                              msg: "Shuffle off");
+                        }
                       },
-                      child: Ink(
+                      child: Obx(
+                        () => Ink(
                           width: 35,
                           height: 35,
                           decoration: BoxDecoration(
-                              color: Theme.of(context).primaryColorDark,
+                              color:
+                                  _playerController.isShuffleModeEnabled.value
+                                      ? Theme.of(context).primaryColor
+                                      : Theme.of(context).primaryColorDark,
                               borderRadius: BorderRadius.circular(50)),
-                          child: Icon(
-                            Icons.shuffle,
-                            color: Theme.of(context).iconTheme.color,
-                            size: 20,
-                          )),
+                          child: _playerController.isShuffleModeEnabled.value
+                              ? Icon(
+                                  Icons.shuffle,
+                                  size: 16,
+                                  color: getTextColorForBackground(
+                                      Theme.of(context).primaryColor),
+                                )
+                              : Icon(
+                                  Icons.shuffle,
+                                  size: 16,
+                                  color: Theme.of(context).iconTheme.color,
+                                ),
+                        ),
+                      ),
                     ),
 
                     const SizedBox(width: 8),
@@ -113,7 +145,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         if (_playerController.mediaItemsInitial.isNotEmpty) {
                           //  start playing the first song
                           //  TODO Play song index saved in the database
-                          _playerController.playSong(index: 0);
+                          _playerController.playSong();
                         }
                       },
                       child: Ink(
@@ -161,74 +193,83 @@ class _HomeScreenState extends State<HomeScreen> {
                       itemScrollController: _scrollController,
                       physics: const BouncingScrollPhysics(),
                       indexBarOptions: IndexBarOptions(
-                        needRebuild: true,
-                        textStyle: Theme.of(context).textTheme.bodySmall!,
-                        selectTextStyle: TextStyle(
-                          fontSize: Theme.of(context).textTheme.bodySmall?.fontSize,
-                          fontWeight: Theme.of(context).textTheme.bodySmall?.fontWeight,
-                          color: Colors.black
-                        ),
-                        selectItemDecoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          color: Theme.of(context).primaryColor
-                        ),
-                        indexHintAlignment: Alignment.centerRight,
-                        indexHintOffset: Offset(-15, 0)
-                      ),
+                          needRebuild: true,
+                          textStyle: Theme.of(context).textTheme.bodySmall!,
+                          selectTextStyle: TextStyle(
+                              fontSize: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall
+                                  ?.fontSize,
+                              fontWeight: Theme.of(context)
+                                  .textTheme
+                                  .bodySmall
+                                  ?.fontWeight,
+                              color: Colors.black),
+                          selectItemDecoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(16),
+                              color: Theme.of(context).primaryColor),
+                          indexHintAlignment: Alignment.centerRight,
+                          indexHintOffset: Offset(-15, 0)),
                       indexBarMargin: EdgeInsets.symmetric(vertical: 16),
                       indexHintBuilder: (context, hint) => Container(
-                        width: 60,
-                        height: 60,
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).primaryColor,
-                          borderRadius: BorderRadius.circular(16)
-                        ),
-                        child: Center(
-                          child: Text(hint, style: TextStyle(
-                            fontWeight: Theme.of(context).textTheme.titleLarge?.fontWeight,
-                            fontSize: Theme.of(context).textTheme.titleMedium?.fontSize,
-                            color: Colors.black
-                          )),
-                        ),
-                      ),
+                            width: 60,
+                            height: 60,
+                            decoration: BoxDecoration(
+                                color: Theme.of(context).primaryColor,
+                                borderRadius: BorderRadius.circular(16)),
+                            child: Center(
+                              child: Text(hint,
+                                  style: TextStyle(
+                                      fontWeight: Theme.of(context)
+                                          .textTheme
+                                          .titleLarge
+                                          ?.fontWeight,
+                                      fontSize: Theme.of(context)
+                                          .textTheme
+                                          .titleMedium
+                                          ?.fontSize,
+                                      color: Colors.black)),
+                            ),
+                          ),
                       itemCount: _playerController.mediaItemsInitial.length,
                       itemBuilder: (context, index) => Padding(
-                        padding:
-                        const EdgeInsets.only(top: 8.0, bottom: 8, left: 8, right: 24),
-                        child: SongCard(
-                          song: _playerController.mediaItemsInitial[index],
-                          songIndex: index,
-                          coreController: _coreController,
-                          playerController: _playerController,
-                          onSongTapped: () {
+                            padding: const EdgeInsets.only(
+                                top: 8.0, bottom: 8, left: 8, right: 24),
+                            child: SongCard(
+                              song: _playerController.mediaItemsInitial[index],
+                              songIndex: index,
+                              coreController: _coreController,
+                              playerController: _playerController,
+                              onSongTapped: () {
+                                //  DISABLE SHUFFLE MODE
+                                if (_playerController
+                                    .isShuffleModeEnabled.value) {
+                                  _playerController.disableShuffle();
+                                  //  Reenable shuffle
+                                  _playerController.shuffle();
+                                }
 
-                            //  DISABLE SHUFFLE MODE
-                            if (_playerController.isShuffleModeEnabled.value) {
-                              _playerController.disableShuffle();
-                              //  Reenable shuffle
-                              _playerController.shuffle();
-                            }
+                                if (_playerController.playerState.value ==
+                                        PlayerStates.playing &&
+                                    _playerController
+                                            .currentPlayingSongIndex.value ==
+                                        index) {
+                                  //  open player screen bottom sheet
+                                  showPlayerBottomSheet(
+                                      playerController: _playerController,
+                                      homeController: _homeController);
+                                } else {
+                                  _playerController.playSongAtIndex(
+                                      index: index);
 
-                            if (_playerController.playerState.value ==
-                                PlayerStates.playing &&
-                                _playerController
-                                    .currentPlayingSongIndex.value ==
-                                    index) {
-                              //  open player screen bottom sheet
-                              showPlayerBottomSheet(
-                                  playerController: _playerController,
-                                  homeController: _homeController);
-                            } else {
-                              _playerController.playSongAtIndex(index: index);
-
-                              //  open player screen bottom sheet
-                              showPlayerBottomSheet(
-                                  playerController: _playerController,
-                                  homeController: _homeController);
-                            }
-                          },
-                        ),
-                      )),
+                                  //  open player screen bottom sheet
+                                  showPlayerBottomSheet(
+                                      playerController: _playerController,
+                                      homeController: _homeController);
+                                }
+                              },
+                            ),
+                          )),
                 );
               },
             )
