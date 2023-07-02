7ecc7f7f459312041d36682ae875807d7377e4a5
KenStarry <starrycodes@gmail.com>
2023-07-02 20:18:34 +0300

    Changed get player prefs use case to use call method

--- lib/core/domain/use_cases/player_use_cases/get_player_prefs_use_case.dart
+++ lib/core/domain/use_cases/player_use_cases/get_player_prefs_use_case.dart
@@ -6,5 +6,5 @@ import '../../models/player_prefs.dart';
 class GetPlayerPrefsUseCase {
   final repo = locator.get<PlayerRepository>();
 
-  Future<PlayerPrefs> invoke() async => await repo.getPlayerPrefs();
+  Future<PlayerPrefs> call() async => await repo.getPlayerPrefs();
 }
