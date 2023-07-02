43572a3d5d3d8cc52040ab70e49bf4a638607a6b
KenStarry <starrycodes@gmail.com>
2023-07-02 20:20:35 +0300

    Changed check permission use case to use call method

--- lib/core/domain/use_cases/player_use_cases/check_permission_use_case.dart
+++ lib/core/domain/use_cases/player_use_cases/check_permission_use_case.dart
@@ -2,9 +2,7 @@ import 'package:flow/di/locator.dart';
 import 'package:flow/core/domain/repository/player_repository.dart';
 
 class CheckPermissionUseCase {
-  final repo = locator.get<PlayerRepository>();
+  final repository = locator.get<PlayerRepository>();
 
-  Future<bool> invoke() async {
-    return await repo.checkPermission();
-  }
+  Future<bool> call() async => await repository.checkPermission();
 }
